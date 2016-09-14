422b25f04dc4f5f35ef4347a66b552d0808296e4
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-09-14 07:10:45 +0200

    move everything to main add event listener for res icon and draw a circle in canvas

--- src/elm/Component/Toolbox.elm
+++ src/elm/Component/Toolbox.elm
@@ -3,6 +3,3 @@ module Component.Toolbox exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-toolbox = div[class "art-toolbox"]
-    [ i[class "art-resistor"][]
-    ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,14 @@
 import Html exposing (..)
 import Html.App exposing (program)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (..)
+import Json.Decode as Json exposing ((:=))
 
-import Component.Toolbox exposing (..)
+import Color exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+import Text
+import Time exposing (..)
 
 main : Program (Never)
 main =
@@ -15,9 +20,11 @@ main =
     }
 
 
-type alias Model = Int
+type alias Model =
+    { schematic : Maybe Form}
 
-init = (0, Cmd.none)
+
+init = ({schematic=Nothing}, Cmd.none)
 
 subscriptions: Model -> Sub Msg
 subscriptions = \_ -> Sub.none
@@ -27,17 +34,18 @@ view model =
   div [class "art-layout"]
     [ div [class "art-main-row"]
         [ div [id "art-toolbox"]
-            [toolbox]
+            [viewToolbox]
         , div [id "art-schematics"]
-            [text "schematics"]
+            [viewSchematic model]
         , div [id "art-explorer"]
-            [text "explorer"]
+            [Html.text "explorer"]
     ]
     , div [id "art-status"]
-        [text "status"]
+        [Html.text "status"]
   ]
 
-type Msg = Increment | Decrement
+type Msg
+    = Schematic
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -47,8 +55,34 @@ update msg model =
 update' : Msg -> Model -> Model
 update' msg model =
   case msg of
-    Increment ->
-      model + 1
+    Schematic ->
+        {model | schematic = Just drawResistor }
 
-    Decrement ->
-      model - 1
+
+viewToolbox = div[class "art-toolbox"]
+    [ i[class "art-resistor",onClick Schematic][]
+    ]
+
+
+
+schematicModel =
+    { conSize=(400,600)
+    , schSize=(390,590)
+    }
+
+viewSchematic model=
+    let
+        (conW, conH) = schematicModel.conSize
+        (schW, schH) = schematicModel.schSize
+    in
+        toHtml <|
+            container conW conH middle <|
+            collage schW schH
+                ([ rect schW schH |> filled (rgb 0 0 0 )]++ (drawElement model.schematic))
+
+drawElement elm =
+    case elm of
+        Nothing ->[]
+        Just _ -> [drawResistor]
+
+drawResistor = circle 20 |> filled (rgb 100 100 100)
